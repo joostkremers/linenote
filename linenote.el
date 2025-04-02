ae5f23174b6be4267ad9b02e904eeb874c16b451
Joost Kremers <joostkremers@fastmail.com>
2025-04-02 16:24:46 +0200

    Update TODO comment.

--- linenote.el
+++ linenote.el
@@ -421,9 +421,9 @@ Pop up a buffer and select it, unless KEEP-FOCUS is non-nil."
            ;; TODO `file-notify-add-watch' triggers an error if the file
            ;; cannot be watched. We should probably handle this.
 
-           ;; TODO This sets up multiple file watchers for the same
-           ;; directory if we annotate more than one file in it. We should
-           ;; probably handle that better.
+           ;; TODO This sets up multiple file watchers for the same node
+           ;; directory if we annotate more than one file in the same
+           ;; source directory. We should probably handle that better.
            (watch-id (file-notify-add-watch note-dir
                                             '(change)
                                             #'linenote--file-changed)))
