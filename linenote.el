6a8e2360b8fc26f77650b187d721af31aff6af9a
Joost Kremers <joostkremers@fastmail.com>
2025-04-16 09:04:38 +0200

    Call `linenote--adjust-all-notes` when disabling linenote-mode.

--- linenote.el
+++ linenote.el
@@ -561,6 +561,7 @@ the tags, if any.."
   (remove-hook 'before-revert-hook #'linenote--remove-all-marks :local)
   (remove-hook 'after-save-hook #'linenote--adjust-all-notes :local)
 
+  (linenote--adjust-all-notes)
   (linenote--save-tags)
   (linenote--remove-all-marks)
   (linenote--dealloc-fswatch))
